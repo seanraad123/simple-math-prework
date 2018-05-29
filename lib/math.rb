ffc3656e4823699dfce59f19bf42d87a6dff849b
Sean Conrad <seanconrad123@gmail.com>
2018-05-29 01:21:52 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,19 @@
 def addition(num1, num2)
+  return num1 + num2
 end
 
+puts (addition(5,4))
+
 def subtraction(num1, num2)
+  return num1 - num2
 end
 
 def division(num1, num2)
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
+  return num1 * num2
 end
 
 def modulo(num1, num2)
